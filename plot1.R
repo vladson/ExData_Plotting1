dec6405ab9612302e49012bee2f5373fac9d390d
Vladislav Bogomolov <vladson4ik@gmail.com>
2014-10-12 02:19:58 +0400

    Plot 1

---
+++ plot1.R
@@ -0,0 +1,11 @@
+datum <- read.table("./household_power_consumption.txt", sep=";", header=T,
+              colClasses = c('character', 'character', 'numeric',
+                             'numeric', 'numeric', 'numeric', 'numeric', 'numeric',
+                             'numeric'), na.strings='?')
+datum$DateTime <- strptime(paste(datum$Date, datum$Time), "%d/%m/%Y %H:%M:%S")
+data <- subset(datum, as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
+# Make devise and start recording
+png("plot1.png", height=480, width=480)
+hist(data$Global_active_power, col='red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power')
+#close devise
+dev.off()
